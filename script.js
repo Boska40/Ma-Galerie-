4856d488280db2ea02fca9a8342acd58bceba508
Boska40 <stephaneboska@gmail.com>
2023-11-24 12:49:52 +0100

    ok

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+// slider
+
+
+
+// fonction des onglets
+function openOnglet(evt,name){
+    let i, tabcontent, tablinks;
+    tabcontent=document.getElementsByClassName('tabcontent');
+    for(i=0; i<tabcontent.length; i++){
+        tabcontent[i].style.display='none';
+       }
+       tablinks=document.getElementsByClassName('tablinks');
+       for(i=0;i<tablinks.length; i++){
+        tablinks[i].className=tablinks[i].className.replace(" active","")
+       }
+       document.getElementById(name).style.display='block';
+       evt.currentTarget.className+=" active";
+}
+
+
+//menu vertical
+function menuDrop() {
+    document.getElementById('dropMenu').classList.add('open');
+}
+
+function menuClose(){
+    document.getElementById('dropMenu').classList.remove('open');
+}
